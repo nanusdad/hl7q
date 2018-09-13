794e62d9389eae7104083b6258f2bdda820c9c0d
Binumon Samuel <binumon.samuel@onehealth.solutions>
2018-09-13 12:38:08 +0530

    modified socket code in index.js

--- index.js
+++ index.js
@@ -6,80 +6,112 @@ var textChunk = '';
 var DDPClient = require("ddp");
 
 var ddpclient = new DDPClient({
-	// All properties optional, defaults shown
-	host: "localhost",
-	port: 3000,
-	ssl: false,
-	autoReconnect: true,
-	autoReconnectTimer: 500,
-	maintainCollections: true,
-	ddpVersion: '1', // ['1', 'pre2', 'pre1'] available
-	// uses the SockJs protocol to create the connection
-	// this still uses websockets, but allows to get the benefits
-	// from projects like meteorhacks:cluster
-	// (for load balancing and service discovery)
-	// do not use `path` option when you are using useSockJs
-	useSockJs: true
-	// Use a full url instead of a set of `host`, `port` and `ssl`
-	// do not set `useSockJs` option if `url` is used
-	//url: 'wss://example.com/websocket'
+    // All properties optional, defaults shown
+    host: "localhost",
+    port: 3000,
+    ssl: false,
+    autoReconnect: true,
+    autoReconnectTimer: 500,
+    maintainCollections: true,
+    ddpVersion: '1', // ['1', 'pre2', 'pre1'] available
+    // uses the SockJs protocol to create the connection
+    // this still uses websockets, but allows to get the benefits
+    // from projects like meteorhacks:cluster
+    // (for load balancing and service discovery)
+    // do not use `path` option when you are using useSockJs
+    useSockJs: true
+            // Use a full url instead of a set of `host`, `port` and `ssl`
+            // do not set `useSockJs` option if `url` is used
+            //url: 'wss://example.com/websocket'
 });
 
-var server = net.createServer(function(socket) {
-	socket.write('Echo server\r\n');
-	socket.on('data', function(data){
-		//console.log(data);
-		textChunk = data.toString('utf8');
-		console.log(textChunk);
-		socket.write(textChunk);
-		
-        var subTextChunk = textChunk.split('♂');
-        for (var i = 1; i < subTextChunk.length; i++) {
-        const parsed_json_data = parser.decode(subTextChunk[i], s12Mapping);
-        console.log('----------------------------PARSED JSON DATA------------------------------');
-        console.log(parsed_json_data);
-        console.log('--------------------------------------------------------------------------');
-        callDDP('addAlert', parsed_json_data);
-     }			
-	});
+var server = net.createServer(function (socket) {
+    socket.write('Echo server\r\n');
+//    socket.on('data', function (data) {
+//        //console.log(data);
+//        textChunk = data.toString('utf8');
+//        //console.log(textChunk);
+//        socket.write(textChunk);
+//        var textChunkTrim = textChunk.substring(textChunk.indexOf("♂"));
+//        textChunkTrim.trim();
+//
+//        subTextChunk = textChunkTrim.split('♂');
+//        for (var i = 0; i < subTextChunk.length; i++) {
+//            const parsed_json_data = parser.decode(subTextChunk[i], s12Mapping);
+//            console.log('----------------------------PARSED JSON DATA------------------------------');
+//            console.log(parsed_json_data);
+//            console.log('--------------------------------------------------------------------------');
+//            callDDP('addAlert', parsed_json_data);
+//        }
+//    });
+
+    var textChunk = "";
+    socket.on('data', function (data) {
+        textChunk += data.toString('utf8'); // Add string on the end of the variable 'chunk'
+        socket.write(textChunk);
+        d_index = textChunk.indexOf('♂'); // Find the delimiter       
+        console.log('d_index : ', d_index);
+
+        // While loop to keep going until no delimiter can be found
+        while (d_index > -1) {
+            try {
+                subTextChunk = textChunk.substring(0, d_index); // Create string up until the delimiter                
+                console.log('subTextChunk : ', subTextChunk);
+                if (subTextChunk) {                    
+                    const parsed_json_data = parser.decode(subTextChunk, s12Mapping);
+                    console.log('----------------------------PARSED JSON DATA------------------------------');
+                    console.log(parsed_json_data);
+                    console.log('--------------------------------------------------------------------------');
+                    callDDP('addAlert', parsed_json_data);
+                }
+            } catch (ex) {
+                console.log(ex);
+            }
+
+            textChunk = textChunk.substring(d_index + 1); // Cuts off the processed chunk
+            console.log('&&&&&&&&&&& ', textChunk);
+            d_index = textChunk.indexOf('♂'); // Find the new delimiter
+        }
+    });
 });
-server.listen(3300, '127.0.0.1');
-//server.listen(3300, '10.1.40.152');//My laptop IP in Masimo's network
+//server.listen(3300, '127.0.0.1');     //localhost
+//server.listen(3300, '10.1.40.152');   //One Health laptop IP in Masimo's network
+server.listen(3300, '104.236.118.123'); //One Health server IP
 
 
 function callDDP(methodname, parameters) {
-	ddpclient.connect(function (error, wasReconnect) {
-		// If autoReconnect is true, this callback will be invoked each time
-		// a server connection is re-established
-		if (error) {
-			console.log('DDP connection error!');
-			return;
-		} else {
-			console.log('DDP connection success!');
-		}
+    ddpclient.connect(function (error, wasReconnect) {
+        // If autoReconnect is true, this callback will be invoked each time
+        // a server connection is re-established
+        if (error) {
+            console.log('DDP connection error!');
+            return;
+        } else {
+            console.log('DDP connection success!');
+        }
 
-		if (wasReconnect) {
-			console.log('Reestablishment of a connection.');
-		}
+        if (wasReconnect) {
+            console.log('Reestablishment of a connection.');
+        }
 
-		console.log('connected!');
-		console.log(methodname, parameters);
+        console.log('connected!');
+        console.log(methodname, parameters);
 
-		setTimeout(function () {
-			/*
-			 * Call a Meteor Method
-			 */
-			ddpclient.call(
-				methodname, // name of Meteor Method being called
-				[parameters], // parameters to send to Meteor Method
-				function (err, result) { // callback which returns the method call results
-					console.log('called method ' + methodname + ' result: ' + result);
-				},
-				function () { // callback which fires when server has finished
-					console.log('updated'); // sending any updated documents as a result of
-					ddpclient.close();
-				}
-			);
-		}, 3000);
-	});
+        setTimeout(function () {
+            /*
+             * Call a Meteor Method
+             */
+            ddpclient.call(
+                    methodname, // name of Meteor Method being called
+                    [parameters], // parameters to send to Meteor Method
+                    function (err, result) { // callback which returns the method call results
+                        console.log('called method ' + methodname + ' result: ' + result);
+                    },
+                    function () { // callback which fires when server has finished
+                        console.log('updated'); // sending any updated documents as a result of
+                        ddpclient.close();
+                    }
+            );
+        }, 3000);
+    });
 }
