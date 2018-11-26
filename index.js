9460f84ad8b1774e05071712463427b701ccc54f
Binumon Samuel <binumon.samuel@onehealth.solutions>
2018-11-26 17:24:36 +0530

    Alarm states check

--- index.js
+++ index.js
@@ -20,33 +20,30 @@ var ddpclient = new DDPClient({
     // (for load balancing and service discovery)
     // do not use `path` option when you are using useSockJs
     useSockJs: true
-            // Use a full url instead of a set of `host`, `port` and `ssl`
-            // do not set `useSockJs` option if `url` is used
-            //url: 'wss://example.com/websocket'
+    // Use a full url instead of a set of `host`, `port` and `ssl`
+    // do not set `useSockJs` option if `url` is used
+    //url: 'wss://example.com/websocket'
 });
 
 var server = net.createServer(function (socket) {
     socket.write('Echo server\r\n');
     socket.on('data', function (data) {
-        textChunk += data.toString('utf8'); // Add string on the end of the variable 'textChunk'
-        console.log('\n textChunk : \n',textChunk);       
+        textChunk += data.toString('utf8'); // Add string on the end of the variable 'textChunk'   		
         d_index = textChunk.indexOf('\u000b'); // Find the delimiter '♂ or \u000b'       
-
         // While loop to keep going until no delimiter can be found
         while (d_index > -1) {
             try {
                 subTextChunk = textChunk.substring(0, d_index); // Create string up until the delimiter                
-                console.log('\n subTextChunk : \n', subTextChunk);                
                 if (subTextChunk) {
                     const parsed_json_data = parser.decode(subTextChunk, s12Mapping);
                     console.log('----------------------------PARSED JSON DATA------------------------------');
                     console.log(parsed_json_data);
                     console.log('--------------------------------------------------------------------------');
-					if (parsed_json_data.OBX3 && parsed_json_data.OBX4) {
-						if (parsed_json_data.OBX3.parameter_name && parsed_json_data.OBX4.value) {
-							callDDP('addAlert', parsed_json_data);
-						}
-					}
+                    if (parsed_json_data.OBX3 && parsed_json_data.OBX4) {
+                        if (parsed_json_data.OBX3.parameter_name && parsed_json_data.OBX4.value) {
+                            callDDP('addAlert', parsed_json_data);
+                        }
+                    }
                 }
             } catch (ex) {
                 console.log(ex);
@@ -59,8 +56,8 @@ var server = net.createServer(function (socket) {
     });
 });
 //server.listen(3300, '127.0.0.1');     //localhost
-server.listen(3300, '104.236.118.123');   //One Health server IP to be set in SafetyNet
-//server.listen(3300, '10.1.40.152');   //One Health laptop IP set in Masimo's SafetyNet
+//server.listen(3300, '104.236.118.123');   //One Health server IP to be set in SafetyNet
+server.listen(3300, '10.1.40.152');   //One Health laptop IP set in Masimo's SafetyNet
 
 function callDDP(methodname, parameters) {
     ddpclient.connect(function (error, wasReconnect) {
@@ -80,15 +77,15 @@ function callDDP(methodname, parameters) {
         setTimeout(function () {
             /* Call a Meteor Method */
             ddpclient.call(
-                    methodname, // name of Meteor Method being called
-                    [parameters], // parameters to send to Meteor Method
-                    function (err, result) { // callback which returns the method call results
-                        console.log('called method ' + methodname);
-                    },
-                    function () { // callback which fires when server has finished
-                        console.log('updated'); // sending any updated documents as a result of
-                        ddpclient.close();
-                    }
+                methodname, // name of Meteor Method being called
+                [parameters], // parameters to send to Meteor Method
+                function (err, result) { // callback which returns the method call results
+                    console.log('called method ' + methodname);
+                },
+                function () { // callback which fires when server has finished
+                    console.log('updated'); // sending any updated documents as a result of
+                    ddpclient.close();
+                }
             );
         }, 3000);
     });
